47b79c4a6b570a06ea677afe5aae53c948fdc09e
Nuz <139971981+Nuzhat-Fatima@users.noreply.github.com>
2023-09-27 16:05:28 +0500

    Create if_else.py

---
+++ if_else.py
@@ -0,0 +1,55 @@
+#In Python there are 3 forms of if....else statement
+#1:if... statement
+#2:if...else statement
+#3:if...elif...else statement
+
+#1:Python if statement
+
+
+"""
+x = 10
+if x > 0:
+    print("Number is positive") #prints "Number is positive" because x is > 0
+"""
+
+#2.if..else statement  
+"""
+y = -20
+if y > 0:
+    print("Positive number")
+else:
+    print("Negative number") 
+"""    
+
+#3.if..elif..else statement
+"""
+z = 0
+if z > 0:
+    print("Positive number")
+elif z == 0:
+    print("Number is zero")
+else:
+    print("Number is negative")
+"""                     
+
+#Python Nested if Statement
+"""
+number = -6
+if number >= 0:
+    if number == 0:
+        print("Number is Zero")
+    print("Number is positive")
+else:
+    print("Number is negative")  """
+
+"""
+is_raining = True
+is_cold = False
+
+if is_raining:
+    print("Remember to bring an umbrella.")
+    if is_cold:
+        print("Also, wear a warm jacket.")
+else:
+    print("It's not raining, enjoy your day!")"""
+
